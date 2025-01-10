851c5efd8f08a41f697d3b90a8e23b2b9cea9ed4
Anubis20233 <dima201602016@gmail.com>
2025-01-10 15:26:41 +0200

    register.js

--- pages/register.js
+++ pages/register.js
@@ -1,40 +1,5 @@
-function promiseAfterTimeout(seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(() => resolve(), seconds*1000);
-  });
-}
+<div class="arrow"></div>
+<div class="wheel rotating"></div>
 
-function rotateWheel(degr) {
-  let wheel = document.querySelector('.wheel');
-  wheel.style.transform = 'rotate('+degr+'deg)';
-  return promiseAfterTimeout(3);
-}
-
-function randomDegrees() {
-  let randomFloat = Math.random()*360;
-  let descreetDegrees = Math.round(randomFloat / 60) * 60;
-  return descreetDegrees;
-}
-
-function getCurrentColor(currentDegrees) {
-  let colors = ["green", "red", "orange", "cyan", "yellow", "blue"];
-  let segmentCount = parseInt(currentDegrees/60);
-  let segmentShift = segmentCount % colors.length;
-  
-  return colors[segmentShift];
-}
-
-function launchSpin() {
-  currentRotation += randomDegrees();
-  
-  rotateWheel(currentRotation)
-    .then(() => {
-      let winColor = getCurrentColor(currentRotation);
-      let result = document.querySelector('.result');
-      result.style.backgroundColor = winColor;
-    });
-}
-
-let currentRotation = 0;
-let spinButton = document.querySelector('.spin');
-spinButton.addEventListener('click', launchSpin);
+<button class="spin">Поехали!</button>
+<div class="result"></div>
